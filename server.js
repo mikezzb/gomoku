306ff92d7830871ec480166c8e8e1674d66dc1c9
mikezzb <mikezhou96@gmail.com>
2020-06-07 03:17:12 +0800

    removed server console log

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ var roomno = 1;
 
 io.on('connection', function(socket){
 
-  console.log('a user connected');
+  //console.log('a user connected');
   userCounter++
 
 
   socket.on('disconnect', function(){
-    console.log('User Disconnected');
+    //console.log('User Disconnected');
     socket.leave("room-"+roomno);
     userCounter--
   });
@@ -31,29 +31,27 @@ io.on('connection', function(socket){
       roomno++;
     joiningRoom=specifiedRoomNo||roomno //if user specified a room number to join else assign a number
     socket.join("room-"+joiningRoom);
-    console.log('Joining room'+joiningRoom)
+    //console.log('Joining room'+joiningRoom)
     //Send this event to everyone in the room to update room status and room number.
     io.sockets.in("room-"+joiningRoom).emit('connectToRoom',{roomno:joiningRoom,clientCount:clientCount});
   });
 
-  socket.on('emitUsername',(data)=>{
-    console.log('Emitted username: '+data.username+' in room '+data.roomno)
+  socket.on('emitUsername',(data)=>{// to be integrated in setplayingcolor
+    //console.log('Emitted username: '+data.username+' in room '+data.roomno)
     socket.to("room-"+data.roomno).emit('emitUsername',data.username);// tell opponent your name
   })
 
   socket.on('move', function(move){//move is a JSON with x,y coord and isBlack boolean
-    console.log('Move : x:'+move.x+' y:'+move.y+' isBlack: '+move.isBlack);
+    //console.log('Move : x:'+move.x+' y:'+move.y+' isBlack: '+move.isBlack);
     socket.to("room-"+move.roomno).emit('move',move);// emit move to the room
   });
 
   socket.on('setPlayingColor',(data)=>{
-    console.log('User '+data.username+'Playing: '+data.isBlack);
+    //console.log('User '+data.username+'Playing: '+data.isBlack);
     socket.to("room-"+data.roomno).emit('setPlayingColor',data);// emit move to the room
   })
 
 });
-
-
 
 const port = process.env.PORT || 5000;
 app.use(sslRedirect());
